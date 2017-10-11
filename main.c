88e78e5e9988af092dc9b3c7d125d0503e3499e1
huabingood <huabingood@qq.com>
2017-10-11 17:51:15 +0800

    create new C programme to sstudy the data structure.

---
+++ main.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+// 引入hyw()所在的头文件
+# include "/opt/huabingood/practise/algorithm/CPractise/CPractise.h"
+
+int main() {
+    printf("Hello, World!\n");
+    // 在main()函数中调用hyw()
+    int a = hyw();
+    printf("%d",a);
+    return 0;
+}
