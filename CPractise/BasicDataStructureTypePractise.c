a023a80262bf1527d5ae7c8fa214ea93e23a0bdd
huabingood <huabingood@qq.com>
2017-10-11 17:52:30 +0800

    create new C programme to sstudy the data structure.

---
+++ CPractise/BasicDataStructureTypePractise.c
@@ -0,0 +1,10 @@
+//
+// Created by huabingood on 17-10-11.
+//
+# include <stdio.h>
+
+// 新建函数hyw()
+int hyw(){
+    // printf("aaa");
+    return 1;
+}
---
+++ CPractise/CPractise.h
@@ -0,0 +1,11 @@
+//
+// Created by huabingood on 17-10-11.
+//
+
+#ifndef ALGORITHM_CPRACTISE_H
+#define ALGORITHM_CPRACTISE_H
+
+#endif //ALGORITHM_CPRACTISE_H
+
+// 在头文件中引入hyw()函数
+int hyw();
