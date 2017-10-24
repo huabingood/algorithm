e92af73f7c521081db5daf4c5f972f59b3036828
huabingood <huabingood@qq.com>
2017-10-24 18:15:45 +0800

    指针的练习

--- CPractise/PointerAndSource.c
+++ CPractise/PointerAndSource.c
@@ -2,3 +2,21 @@
 // Created by huabingood on 10/24/17.
 //
 
+# include <stdio.h>
+
+void reversNumber() {
+    int a, b;
+    printf("请输入两个数字：\n");
+    scanf("%d%d", &a, &b);
+    printf("您输入的第一数字是%d,您输入的第二个数字是：%d", a, b);
+
+
+}
+
+void reverse(int *x, int *y) {
+    int *middle = 0;
+
+    // &middle=&x;
+
+
+}
