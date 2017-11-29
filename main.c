e1be767f45bfa6d4c8c038071bcda502c30b2f02
huabingood <huabingood@qq.com>
2017-11-29 14:18:49 +0800

    输入输出练习

--- main.c
+++ main.c
@@ -9,11 +9,85 @@
 
 
 // this is alograthm introduction
-void main() {
+#include <stdlib.h>
+
+int main() {
+    int hour1, hour2, second1, second2, time1, time2, middle, hour3, second3;
+    printf("Input time one(hour, second):");
+    scanf("%d,%d", &hour1, &second1);
+    printf("Input time two(hour, second):");
+    scanf("%d,%d", &hour2, &second2);
+
+    time1 = hour1 * 60 + second1;
+    time2 = hour2 * 60 + second2;
+    middle = abs(time1 - time2);
+
+    second3 = middle % 60;
+    hour3 = (middle - middle % 60) / 60;
+
+    printf("%d hour %d second\n", hour3, second3);
+
+}
+
+
+
+/*
+# define YOUHUI 0.08
+int main(){
+    float p,price;
+    printf("Input payment p:");
+    scanf("%f",&p);
+
+    price = p-(p * YOUHUI);
+
+    printf("price = %.1f\n",price);
+
+}
+*/
+
+
+
+/*int main() {
+    float length, result;
+    int days;
+    int chuShu = 1;
+    printf("Input length and days:");
+    scanf("%f,%d", &length, &days);
+
+    for (int i = 1; i <= days; i++) {
+        chuShu *= 2;
+    }
+
+    result = length / chuShu;
+    printf("ength=%.5f\n", result);
+}*/
+
+
+
+/*
+int main(){
+    int math1,english,math2;
+    float score;
+    printf("Input math1,English and math1:");
+    scanf("%d,%d,%d",&math1,&english,&math2);
+    score = (math1*5 + english*1.5+math2*3.5)/10;
+    printf("Final score = %.2f\n",score);
+}
+*/
+
+
+
+/*int main() {
+    int a, b, s;
+    scanf("%d%d%d",&a,&b,&s);
+    printf("%d%d%d",a,b,s);
+}*/
+
+/*void main() {
 
     // mathod2();
     // mathod1();
-}
+}*/
 
 
 
